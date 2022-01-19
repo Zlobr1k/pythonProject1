415b7310e476f41a28fadb66c6f981f73be0755e
PK-Alex <Sheremetlesha@gmail.com>
2022-01-19 17:39:23 +0200

    test

---
+++ main.py
@@ -0,0 +1,60 @@
+#Задача 1
+
+#import random
+
+#num1 = random.randint(0, 100)
+#num2 = random.randint(0, 100)
+
+#print(num1)
+#print(num2)
+
+
+#if num1 % 2 >= 1 and num2 % 2 >= 1:
+#    print('Все числа не чётные')
+#elif num1 % 2 >= 1:
+#    print(num1)
+#elif num2 % 2 >= 1:
+#    print(num2)
+#else:
+#    print('Все числа чётные')
+
+#Задача 2
+
+#num1 = int(input())
+#num2 = int(input())
+#num3 = int(input())
+
+#if num1 < num2 < num3 or num3 < num2 < num1:
+#    print(num2)
+#elif num2 < num1 < num3 or num3 < num1 < num2:
+#    print(num1)
+#else:
+#    print(num3)
+
+#Задача 5
+
+#num1 = int(input())
+#num2 = int(input())
+#num3 = int(input())
+
+#if num1 < num2 < num3 or num2 < num1 < num3:
+#    print(num3)
+#elif num3 < num1 < num2 or num1 < num3 < num2:
+#    print(num2)
+#else:
+#    print(num1)
+
+#Задача 8
+
+#num1 = int(input())
+#num2 = int(input())
+
+#if num1 < num2:
+#    print('Числа не делятся')
+#elif num1 % num2 == 0:
+#    print(num1, ' делится на ', num2, '\n', num1 / num2)
+#elif num1 % num2 != 0:
+#    print('Остаток: ', num1 % num2)
+
+
+
